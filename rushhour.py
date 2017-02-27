ffa632c5c452efb8bea2eba7fe0a5e6754df9404
Bob Karreman <bob@karreman.org>
2017-02-27 21:17:13 +0100

    Implement BFS algorigthm to search for the solution

--- rushhour.py
+++ rushhour.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+from datetime import datetime
 
 
 class Car(object):
@@ -64,6 +66,48 @@ class Board(object):
             output += "%s\n" % " ".join(row)
         return output
 
+    def slot_free(self, x, y):
+        # print("Slot free:", x, y)
+        """ Checks if a slot is in use by a car """
+        return self.matrix[y][x] == self.EMPTY_CHAR
+
+    def move_car(self, car, x, y):
+        """ Moves the car and returns a new Board """
+        new_cars = set()
+        for c in self.cars:
+            if c is car:
+                new_cars.add(Car(c.name, x, y, c.length, c.orientation))
+            else:
+                new_cars.add(Car(c.name, c.x, c.y, c.length, c.orientation))
+        new_board = Board(new_cars)
+        return new_board
+
+    def possibilities(self):
+        """ Returns a list of boards (moves) that are all possible from the current state of this board """
+        boards = []
+        for car in self.cars:
+            if car.is_horizontal():
+                # Can move left
+                if car.x - 1 >= 0 and self.slot_free(car.x - 1, car.y):
+                    # print("Car %s can move Left" % car.name, car.x-1, car.y)
+                    boards.append(self.move_car(car, car.x - 1, car.y))
+
+                # Can move right
+                if car.x + car.length < self.SIZE and self.slot_free(car.x + car.length, car.y):
+                    # print("Car %s can move Right" % car.name, car.x + 1, car.y)
+                    boards.append(self.move_car(car, car.x + 1, car.y))
+            else:
+                # Can move up
+                if car.y - 1 >= 0 and self.slot_free(car.x, car.y - 1):
+                    # print("Car %s can move Up" % car.name, car.x, car.y -1)
+                    boards.append(self.move_car(car, car.x, car.y - 1))
+
+                # Can move down
+                if car.y + car.length < self.SIZE and self.slot_free(car.x, car.y + car.length):
+                    # print("Car %s can move Down" % car.name, car.x, car.y + 1)
+                    boards.append(self.move_car(car, car.x, car.y + 1))
+        return boards
+
 
 class Rushhour(object):
     def __init__(self):
@@ -88,6 +132,45 @@ class Rushhour(object):
                             car.length += 1
             return list(car_map.values())
 
+    @staticmethod
+    def find_solution(board):
+        """
+        Use a BFS (bread first search) algorithm to find the shortest path to the solution for the given board.
+
+        An some other algorithms that could be used are A* or Dijkstra
+        Which adds more complexity, that could be used to add a weight to certain moves but not needed in this case.
+        ref: http://www.redblobgames.com/pathfinding/a-star/implementation.html
+        """
+        # TODO: Keep track of the path to the solution
+        seen = set()
+
+        queue = deque()
+        queue.append(board)
+
+        idx = 0
+        while len(queue) > 0:
+            # Get first item from the queue
+            board = queue.popleft()
+
+            # If board is already seen skip it (there was a faster way to get to that board)
+            if str(board) in seen:
+                continue
+
+            seen.add(str(board))
+
+            if board.is_finished():
+                print("We found a solution by checking %s boards!" % idx)
+                return True
+
+            new_boards = board.possibilities()
+            queue.extend(new_boards)
+
+            idx += 1
+            if idx > 100000:
+                print('More then 100000 moves stopping')
+                break
+        return False
+
 
 def main(path):
     cars = Rushhour.parse_file(path)
@@ -96,6 +179,14 @@ def main(path):
     print("Board:")
     print(board.print_board())
 
+    start = datetime.now()
+    is_finished = Rushhour.find_solution(board)
+    delta = datetime.now() - start
+
+    if is_finished:
+        print("Solution found in %s seconds" % delta.total_seconds())
+    else:
+        print('No solution found :-(')
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
